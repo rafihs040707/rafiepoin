219692aafe306436db48d8d97de400ded05219fb
nidazhr <nida.khoiriyah2007@smkn4-tsm.sch.id>
2024-12-02 11:18:41 +0700

    penambahan css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,24 @@
 @extends('auth.layouts')
 
 @section('content')
-
-<h1>Login</h1>
-<a href="{{ route('register') }}">Daftar</a>
 <br><br>
 <form action="{{ route('authenticate') }}" method="post">
+<div class="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg grid justify-center">
+      <h1 class="text-xl text-center font-sans font-medium">Login</h1><br>
       @csrf
-      <label>Email Address</label><br>
-      <input type="email" id="email" name="email" value="{{ old('email') }}"><br><br>
-      <label>Password</label><br>
-      <input type="password" id="password" name="password"><br><br>
-      <input type="submit" value="Login">
+      <label>Email Address</label>
+      <input type="email" id="email" name="email" value="{{ old('email') }}" class="border border-slate-400 rounded bg-white hover:bg-sky-50 py-1"><br>
+      
+      <label>Password</label>
+      <input type="password" id="password" name="password" class="border border-slate-400 rounded bg-white hover:bg-sky-50 py-1 px-3"><br>
+      
+      <input type="submit" value="Login" class="bg-gray-950 hover:bg-gray-400 border text-white border-slate-400 rounded">
+
+      <div class="flex justify-center py-2">
+      <p>Apabila belum punya akun!</p>
+      <a href="{{ route('register') }}" class="text-blue-600 px-1">Daftar</a>
+      </div>
+</div>
 </form>
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,45 @@
 @extends('auth.layouts')
 
 @section('content')
-<h1>Register</h1>
-<a href="{{ route('login') }}">Login</a>
 <br><br>
 <form action="{{ route('store') }}" method="POST">
-
+<div class="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg grid justify-center">
+      <h1 class="text-xl text-center font-sans font-medium">Register</h1><br>
       @csrf
-      <label>Nama Lengkap</label><br>
-      <input type="text" id="name" name="name" value="{{ old('name') }}"><br>
+      <label>Nama Lengkap</label>
+      <input type="text" id="name" name="name" value="{{ old('name') }}" class="border border-slate-400 rounded bg-white hover:bg-sky-50"><br>
       
       @if ($errors->has('name'))
       <span class="text-danger">{{ $errors->first("name") }}</span>
       @endif
 
-      <br>
-      <label>Email Address</label><br>
-      <input type="email" id="email" name="email" value="{{ old('email') }}"><br>
+      
+      <label>Email Address</label>
+      <input type="email" id="email" name="email" value="{{ old('email') }}" class="border border-slate-400 rounded bg-white hover:bg-sky-50"><br>
 
       @if ($errors->has('email'))
       <span class="text-danger">{{ $errors->first("email") }}</span>
       @endif
 
-      <br>
-      <label>Password</label><br>
-      <input type="password" id="password" name="password"><br>
+      
+      <label>Password</label>
+      <input type="password" id="password" name="password" class="border border-slate-400 rounded bg-white hover:bg-sky-50"><br>
 
       @if ($errors->has('password'))
       <span class="text-danger">{{ $errors->first("password") }}</span>
       @endif
 
-      <br>
+      
       <label for="password_confirmation" class="col-md-4 col-form-label text-md-end text-start">Confirm Password</label>
       <div class="col-md-6">
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+            <input type="password" class="form-control border border-slate-400 rounded bg-white hover:bg-sky-50" id="password_confirmation" name="password_confirmation">
+      </div> <br>
+      <input type="submit" value="Register" class=" bg-gray-950 hover:bg-gray-400 border text-white border-slate-400 rounded">
+
+      <div class="flex justify-center py-2">
+      <p>Apabila sudah punya akun!</p>
+      <a href="{{ route('login') }}" class="text-blue-600 px-1">Login</a>
       </div>
-      <input type="submit" value="register">
+</div>
 </form>
 @endsection
