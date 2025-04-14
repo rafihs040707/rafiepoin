b1328f76bc823b81ba8fc9628b9045407b4fdd03
Tejar456 <tejarm04@gmail.com>
2025-04-14 13:51:40 +0700

    Perubahan

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -5,12 +5,10 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <script src="https://cdn.tailwindcss.com"></script>
       <title>Laravel 11</title>
-</head>
 
-<body class="flex-items-center grid justify-center min-h-screen">
-      <div class="container flex-items-center grid justify-center min-h-screen">
+<body>
+      <div class="container">
             @yield('content')
       </div>
 </body>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,15 @@
 @extends('auth.layouts')
 
 @section('content')
-<br><br>
-<form action="{{ route('authenticate') }}" method="post">
-<div class="group block max-w-max mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg grid justify-center">
-      <h1 class="text-xl text-center font-sans font-medium">Login</h1><br>
-      @csrf
-      <label>Email Address</label>
-      <input type="email" id="email" name="email" value="{{ old('email') }}" class="border border-slate-400 rounded bg-white hover:bg-sky-50 py-1"><br>
-      
-      <label>Password</label>
-      <input type="password" id="password" name="password" class="border border-slate-400 rounded bg-white hover:bg-sky-50 py-1 px-3"><br>
-      
-      <input type="submit" value="Login" class="bg-gray-950 hover:bg-gray-400 border text-white border-slate-400 rounded">
 
-</div>
+<form action="{{ route('authenticate') }}" method="post">
+      <h1>Login</h1><br>
+      @csrf
+      <label>Email Address</label><br>
+      <input type="email" id="email" name="email" value="{{ old('email') }}"><br><br>
+      <label>Password</label><br>
+      <input type="password" id="password" name="password"><br><br>
+      <input type="submit" value="Login">
 </form>
 
 @endsection
